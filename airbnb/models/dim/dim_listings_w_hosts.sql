1bc8169d56b0734971ad70f9617d173559d3d47d
michael rasmussen <michaelrasmussen@gmail.com>
2026-03-13 14:46:31 -0500

    formatting update

--- airbnb/models/dim/dim_listings_w_hosts.sql
+++ airbnb/models/dim/dim_listings_w_hosts.sql
@@ -1,10 +1,10 @@
 WITH
 l AS (
-    SELECT
-        *
+    SELECT *
     FROM
         {{ ref('dim_listings_cleansed') }}
 ),
+
 h AS (
     SELECT *
     FROM {{ ref('dim_hosts_cleansed') }}
@@ -18,8 +18,8 @@ SELECT
     l.price,
     l.host_id,
     h.host_name,
-    h.is_superhost as host_is_superhost,
+    h.is_superhost AS host_is_superhost,
     l.created_at,
-    GREATEST(l.updated_at, h.updated_at) as updated_at
+    GREATEST(l.updated_at, h.updated_at) AS updated_at
 FROM l
-LEFT JOIN h ON (h.host_id = l.host_id)
+LEFT JOIN h ON (l.host_id = h.host_id)
